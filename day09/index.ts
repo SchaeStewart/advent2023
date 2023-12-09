af1ddf95c7bd4437875feda3c7985f50d6364d75
Schaffer Stewart <schaestewart@gmail.com>
2023-12-09 13:42:49 -0500

    day09. refactor

--- day09/index.ts
+++ day09/index.ts
@@ -19,34 +19,27 @@ const getDifferences = (nums: number[]) => {
   return result;
 };
 
-const reduceSequence = (nums: number[]): number => {
-  const diffs = getDifferences(nums);
-  if (diffs.every((d) => d === 0)) {
-    return nums.at(-1)! - diffs.at(-1)!;
-  }
-  const n = reduceSequence(diffs);
-  return nums.at(-1)! + n;
-};
-
-const reduceSequenceBackward = (nums: number[]): number => {
-  const diffs = getDifferences(nums);
-  if (diffs.every((d) => d === 0)) {
-    return nums.at(0)! - diffs.at(0)!;
-  }
-  const n = reduceSequenceBackward(diffs);
-  return nums.at(0)! - n;
-};
+const reduceSequence =
+  (dir: -1 | 0, op: (a: number, b: number) => number) =>
+  (nums: number[]): number => {
+    const diffs = getDifferences(nums);
+    if (diffs.every((d) => d === 0)) {
+      return nums.at(dir)! - diffs.at(dir)!;
+    }
+    const n = reduceSequence(dir, op)(diffs);
+    return op(nums.at(dir)!, n);
+  };
 
 const part1 = async () => {
   const data = parseInput(await readInput(input));
-  const diffs = data.map(reduceSequence);
+  const diffs = data.map(reduceSequence(-1, (a, b) => a + b));
   const sum = diffs.reduce((acc, val) => acc + val, 0);
   console.log("Part 1", { sum });
 };
 
 const part2 = async () => {
   const data = parseInput(await readInput(input));
-  const diffs = data.map(reduceSequenceBackward);
+  const diffs = data.map(reduceSequence(0, (a, b) => a - b));
   const sum = diffs.reduce((acc, val) => acc + val, 0);
 
   console.log("Part 2", { sum });
@@ -54,5 +47,5 @@ const part2 = async () => {
 
 await part1();
 await part2();
-// 1877825184 -
+// 1877825184
 // 1108
